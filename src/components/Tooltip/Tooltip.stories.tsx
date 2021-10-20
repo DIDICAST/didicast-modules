546fafe053272a661e5e3df0f0c388645f73ce4e
Je_Ji_Yo <hoha387782@gmail.com>
2021-10-20 18:32:02 +0900

    툴팁 모듈 소개 1차

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,9 +1,57 @@
+import { Box, Typography } from "@material-ui/core";
+import {
+  ArgsTable,
+  Canvas,
+  Description,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
 import { Meta, Story } from "@storybook/react";
 import Tooltip, { TooltipProps } from "./Tooltip";
 
 export default {
   title: "Components/Tooltip",
   component: Tooltip,
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Canvas>
+            <div>
+              <Typography variant="h6" gutterBottom component="div">
+                아래 물음표 위로 마우스를 가져가면 툴팁을 볼 수 있습니다.
+              </Typography>
+              <Box
+                sx={{
+                  width: 300,
+                  height: 300,
+                  p: 16,
+                  border: "1px dashed grey",
+                }}
+              >
+                <Tooltip
+                  content={
+                    <>
+                      <div>
+                        <span>마우스를 이동하면 사라집니다.</span>
+                      </div>
+                    </>
+                  }
+                />
+              </Box>
+            </div>
+          </Canvas>
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories includePrimary />
+        </>
+      ),
+    },
+  },
 } as Meta;
 
 const Template: Story<TooltipProps> = (args) => <Tooltip {...args} />;
