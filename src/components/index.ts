2bd17928eccd57f0e653b067c5fa0a6046bca691
Scott Ko <swko1017@naver.com>
2021-11-10 17:45:44 +0900

    Revert "modal 모듈 추가"
    This reverts commit d6f4354fc724cf62631d15d4dfadcc448052ac0d.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from "./Button";
-export { default as Modal } from "./Modal";
 export { default as Select } from "./Select";
 export { default as Tooltip } from "./Tooltip";
 export { default as Title } from "./Title";
